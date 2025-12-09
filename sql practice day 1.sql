25eebb2034e3a602277775298b43b80461c7ebcf
akanksha806 <230435251+akanksha806@users.noreply.github.com>
2025-12-09 10:47:41 +0530

    Add files via upload
    basic statement

---
+++ sql practice day 1.sql
@@ -0,0 +1,29 @@
+show databases;
+use mysql;
+
+create database akanksha_gupta;
+show databases;
+use akanksha_gupta;
+-- to see output select statement
+select database();
+show tables; -- display tables
+create table akshu(salary int , age int);
+show tables ;
+-- display information of a 
+*/
+describe akshu ;
+-- to see databases
+show databases ;
+-- to print current_databases;
+select database();
+-- to work with database:
+use database:
+-- to see tables
+show tables;
+-- create table
+create table table_name(column datatype)
+-- table (description) -- too see the column name and datatype
+describe table_name;
+*/
+
+
